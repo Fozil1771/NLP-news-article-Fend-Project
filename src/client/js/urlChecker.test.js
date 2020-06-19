6fd6feafc845aa4a22ae6f5a56942e04d36bb5d4
Fozil1771 <51485736+Fozil1771@users.noreply.github.com>
2020-06-19 12:45:03 +0500

    Update urlChecker.test.js

--- src/client/js/urlChecker.test.js
+++ src/client/js/urlChecker.test.js
@@ -1,6 +1,4 @@
-
 import {checkForUrl,} from './urlChecker';
-
 
 test("It should return true", async () => {
         expect(checkForUrl).toBeDefined();
@@ -8,22 +6,3 @@ test("It should return true", async () => {
 test("It should be a function", async () => {
         expect(typeof checkForUrl).toBe("function");
 });
-
-// describe('Test, the function "checkForUrl()" for valid url' , () => {
-//     var url = "https://edition.cnn.com/2020/06/18/tennis/dylan-alcott-wheelchair-tennis-us-open-discrimination-spt-intl/index.html";
-//     test('It should return true', async () => {
-//         const response = validURL(url);
-//         expect(response).toBeDefined();
-//         expect(response).toBe(true);
-//     });
-// });
-// describe('Test "checkForUrl()" for invalid url' , () => {
-//     // I replaced 'https' with 'htpshttps' to make it invalid
-//     var url = "htpshttps://edition.cnn.com/2020/06/18/tennis/dylan-alcott-wheelchair-tennis-us-open-discrimination-spt-intl/index.html";
-//     test('It should return false', async () => {
-//         const response = checkForUrl(url);
-//         expect(response).toBeDefined();
-//         expect(response).toBe(false);
-//     });
-// });
-
